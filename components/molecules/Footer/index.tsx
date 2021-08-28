8b414ff58280385869b5dd86e1df0ea33ee5f389
cyh0530 <chiayanghuang0@gmail.com>
2021-08-28 21:01:48 +0800

    feat: add footer

---
+++ components/molecules/Footer/index.tsx
@@ -0,0 +1,38 @@
+import { Icon } from "@iconify/react";
+import Section from "../Section";
+import socialMedia from "@constants/socialMedia";
+
+const Footer = () => {
+  return (
+    <Section id="contact" bg="dark" title="Reach Out to Me!" className="pt-0">
+      <div className="text-center">
+        <p className="lead">
+          I'm currently looking for software engineering opportunities. <br />
+          Feel free to contact me if you have any position available
+        </p>
+        <p className="pt-2">
+          {socialMedia.map((social, index) => (
+            <a
+              href={social.link}
+              key={index}
+              title={social.name}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-dark"
+            >
+              <Icon
+                icon={social.icon}
+                // size="2x"
+                height={32}
+                className="mx-2"
+                color={social.color}
+              />
+            </a>
+          ))}
+        </p>
+      </div>
+    </Section>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
-import Header from '../components/molecules/Header'
-import Main from '../components/organisms/Main'
-
+import Header from "@components/molecules/Header";
+import Main from "@components/organisms/Main";
+import Footer from '@components/molecules/Footer'
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   )
 }
