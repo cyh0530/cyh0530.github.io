11b47476ff5a1b7022068d284120c82f39093333
cyh0530 <44914377+cyh0530@users.noreply.github.com>
2021-08-28 15:39:05 +0000

    Deploying to gh-pages from @ cyh0530/cyh0530.github.io@33bfe4bfb343cbda07161e49dd9419abd3328251 🚀

---
+++ _next/static/4ldOL9J2gOrdvmVpnsA29/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/152-b61170a058c17dc5390a.js","static/chunks/pages/index-8cac3a55899c4d756f94.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
