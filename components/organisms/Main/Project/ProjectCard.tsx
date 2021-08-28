62333295d360f675be7ab9c66e0764f06be5eaa3
cyh0530 <chiayanghuang0@gmail.com>
2021-08-28 21:59:25 +0800

    fix: remove card image height specification

--- components/organisms/Main/Project/ProjectCard.tsx
+++ components/organisms/Main/Project/ProjectCard.tsx
@@ -31,7 +31,7 @@ const ProjectCard = ({
       className="text-body"
     >
       <Card className="shadow-lg p-3 rounded mb-5 bg-white">
-        <Card.Img variant="top" src={image} className="rounded" height={320} />
+        <Card.Img variant="top" src={image} className="rounded"  />
         <Card.Body>
           <Card.Title className="">{title}</Card.Title>
           <Card.Text style={{ minHeight: 100 }}>{description}</Card.Text>
