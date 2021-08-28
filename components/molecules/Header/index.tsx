8778d407661ac67248da1eae64396038bbe5e555
cyh0530 <chiayanghuang0@gmail.com>
2021-08-28 15:52:14 +0800

    feat: add navigation header

---
+++ components/molecules/Header/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+// import SmoothScroll from "smooth-scroll";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from 'react-bootstrap/Container'
+import { LINKS } from "../../../constants";
+
+const Header = () => {
+  useEffect(() => {
+    // new SmoothScroll('a[href*="#"]', {
+    //   speed: 2500,
+    //   speedAsDuration: true,
+    //   easing: "easeInOutCubic",
+    //   updateURL: false,
+    //   header: "#header",
+    // });
+  }, []);
+
+  return (
+    <Navbar
+      bg="light"
+      expand="lg"
+      variant="light"
+      id="header"
+      className="position-fixed start-0 end-0 shadow-sm"
+      style={{ zIndex: 1000 }}
+    >
+      <Container fluid>
+        <Navbar.Brand href="/">cyh0530</Navbar.Brand>
+        <Navbar.Toggle className="toggler" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            {LINKS.map((link, index) => (
+              <Nav.Link
+                key={index}
+                className=""
+                href={`#${link.toLowerCase()}`}
+              >
+                {link}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
