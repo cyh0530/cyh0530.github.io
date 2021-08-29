e03b768565633cc8dba2bbf40187cf8ecc4d4e97
cyh0530 <chiayanghuang0@gmail.com>
2021-08-29 13:38:07 +0800

    feat: add open graph

---
+++ next-seo.config.js
@@ -0,0 +1,28 @@
+import { SITE_DESC } from "./constants";
+
+export default {
+  openGraph: {
+    url: "https://cyh0530.github.io/",
+    type: "website",
+    title: "Chia-Yang Huang",
+    description: SITE_DESC,
+    images: [
+      {
+        url: "https://cyh0530.github.io/static/images/banner.jpg",
+        alt: "Chia-Yang Huang",
+      },
+    ],
+    locale: "en_US",
+    site_name: "Chia-Yang Huang",
+    profile: {
+      firstName: "Chia-Yang",
+      lastName: "Huang",
+      username: "cyh0530",
+      gender: "male",
+    },
+  },
+  twitter: {
+    site: "@cyh0530",
+    cardType: "summary",
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,31 @@
-import Head from "next/head";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as ga from "../lib/ga";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { DefaultSeo } from "next-seo";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as ga from "../lib/ga";
 import { TITLE } from "../constants";
+import SEO from '../next-seo.config'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
 
-    const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageView(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
 
-    useEffect(() => {
-      const handleRouteChange = (url: string) => {
-        ga.pageView(url);
-      };
-      //When the component is mounted, subscribe to router changes
-      //and log those page views
-      router.events.on("routeChangeComplete", handleRouteChange);
-
-      // If the component is unmounted, unsubscribe
-      // from the event with the `off` method
-      return () => {
-        router.events.off("routeChangeComplete", handleRouteChange);
-      };
-    }, [router.events]);
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
@@ -32,6 +33,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>{TITLE}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
   );
