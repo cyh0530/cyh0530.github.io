73baa43cc82d6d34d18db1cdca607f1c8ab48d73
cyh0530 <chiayanghuang0@gmail.com>
2023-06-02 19:14:33 -0700

    chore: add image cache control

--- next.config.js
+++ next.config.js
@@ -1,10 +1,48 @@
 /** @type {import('next').NextConfig} */
-const withImage = require("next-images")
-module.exports = withImage({
-  reactStrictMode: true,
-  esModule: true,
-  fileExtensions: ["jpg", "jpeg", "png", "gif"],
-  webpack(config, options) {
-    return config;
+const withImage = require("next-images");
+
+// max-age = 1 day, stale-while-revalidate = 30 days
+const imageCacheControl =
+  "public, max-age=86400, s-maxage=86400, stale-while-revalidate=2592000";
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*).jpg",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: imageCacheControl,
+          },
+        ],
+      },
+      {
+        source: "/(.*).jpeg",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: imageCacheControl,
+          },
+        ],
+      },
+      {
+        source: "/(.*).png",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: imageCacheControl,
+          },
+        ],
+      },
+    ];
   },
-});
+  ...withImage({
+    reactStrictMode: true,
+    esModule: true,
+    fileExtensions: ["jpg", "jpeg", "png", "gif"],
+    webpack(config, options) {
+      return config;
+    },
+  }),
+};
